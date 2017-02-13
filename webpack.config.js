52242312254e8eeff1f223ddd64f5cfad21b3a12
Edwin P. Magezi <emagezi@gmail.com>
2017-02-13 14:25:04 +0300

    Add project dependencies

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require("webpack");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: "./src/com/mendix/widget/AutoLoadMore/AutoLoadMore.ts",
+    output: {
+        path: path.resolve(__dirname, "dist/tmp"),
+        filename: "src/com/mendix/widget/AutoLoadMore/AutoLoadMore.ts",
+        libraryTarget:  "umd"
+    },
+    resolve: {
+        extensions: [ ".ts", ".js", ".json" ],
+        alias: {
+            "tests": path.resolve(__dirname, "./tests")
+        }
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, use: "ts-loader" },
+            { test: /\.css$/, loader: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            }) }
+        ]
+    },
+    devtool: "source-map",
+    externals: [ "mxui/widget/_WidgetBase", "dojo/_base/declare" ],
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: "src/**/*.js" },
+            { from: "src/**/*.xml" }
+        ], {
+            copyUnmodified: true
+        }),
+        new ExtractTextPlugin({ filename: "./src/com/mendix/widget/AutoLoadMore/ui/AutoLoadMore.css" }),
+        new webpack.LoaderOptionsPlugin({
+            debug: true
+        })
+    ]
+};
